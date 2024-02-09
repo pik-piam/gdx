405d40aac0aa7550972357163b18a98437e8ad58
Michaja Pehl <pehl@pik-potsdam.de>
2024-02-09 11:50:50 +0100

    manually scan the PATH, which igdx does not do on Linux

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,18 +1,24 @@
 #' @importFrom gdxrrw igdx
-#' @importFrom withr with_output_sink
+#' @importFrom utils capture.output
 .onLoad <- function(libname, pkgname) {
-  tmp <- NULL
-  with_output_sink(new = textConnection("tmp", "w", local = TRUE),
-                   code = {
-                     # make igdx try an empty path to load GDX libraries, which
-                     # will fail and igdx will try the path and library search
-                     # mechanisms in turn
-                     ok <- as.logical(igdx(""))
-                   })
+  path <- strsplit(Sys.getenv("PATH"), .Platform$path.sep, fixed = TRUE)[[1]]
+  path <- grep("gams", path, ignore.case = TRUE, value = TRUE)
+  # disregard variables on the Windows path
+  path <- grep("%", path, value = TRUE, fixed = TRUE, invert = TRUE)
+
+  # append GAMSROOT (or empty if that does not exist) to make sure igdx is
+  # called at least once
+  path <- c(path, Sys.getenv("GAMSROOT"))
+
+  for (p in path) {
+    msg <- capture.output(ok <- as.logical(igdx(p)))
+    if (ok)
+      break
+  }
 
   if (!ok) {
     # truncate igdx output to 132 characters per line
-    tmp <- paste0(strtrim(tmp, 129), c('', '...')[(nchar(tmp) > 132) + 1])
-    packageStartupMessage(paste(tmp, collapse = "\n"))
+    msg <- paste0(strtrim(msg, 129), c("", "...")[(nchar(msg) > 132) + 1])
+    packageStartupMessage(paste(msg, collapse = "\n"))
   }
 }
